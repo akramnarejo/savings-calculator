d61bbfe44aeb1b1940183d69122b3d66ca70c943
Akram Narejo <ak.narejo@gmail.com>
2022-04-24 20:06:45 +0500

    feat: calculations.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {findTotalInvestment, findDirectSavings, findSavigsComparison} from './calculations'
 
 
 function App() {
@@ -15,43 +16,92 @@ function App() {
   const [isClicked3, setIsClicked3] = useState(false)
   const [isClicked4, setIsClicked4] = useState(false)
   const [isClicked5, setIsClicked5] = useState(false)
-  
+  const [year, setYear] = useState(0)
+  // input states
+  const [leaseCost, setLeaseCost] = useState(0)
+  const [portfolio, setPortfolio] = useState(0)
+  const [capexSpend, setCapexSpend] = useState(0)
+  const [estNeeds, setEstNeeds] = useState(0)
+  //calculation states
+  const [costAvoidance, setCostAvoidance] = useState(0)
+  const [spaceOptimization, setSpaceOptimization] = useState(0)
+  const [totalInvestment, setTotalInvestment] = useState(0)
+  const [totalSavings, setTotalSavings] = useState(0)
+  const [netSavings, setNetSavings] = useState(0)
+  const [roi, setRoi] = useState(0)
+  const [costSF, setCostSF] = useState(0)
+  const [directSavings, setDirectSavings] = useState(0)
+  const [savingsComparison, setSavingsComparison] = useState(0)
+
+  const calculateSavings = (val) => {
+    setYear(val)
+    setCostAvoidance(0)
+    setCostAvoidance(estNeeds*0.84*leaseCost)
+    setSpaceOptimization(0)
+    setSpaceOptimization((capexSpend*0.025*portfolio)*val)
+    setTotalInvestment(0)
+    setTotalInvestment(findTotalInvestment(portfolio))
+    setTotalSavings(0)
+    setNetSavings(0)
+    setRoi(0)
+    setCostSF(0)
+    setDirectSavings(0)
+    setSavingsComparison(0)
+  }
+
+  useEffect(() => {
+    setTotalSavings(costAvoidance+spaceOptimization)
+    const result = totalSavings - totalInvestment
+    setNetSavings(result > 0 ? result : 0)
+    setRoi(netSavings/totalInvestment)
+    if(totalInvestment){
+      setDirectSavings(findDirectSavings(totalInvestment, portfolio))
+      setSavingsComparison(findSavigsComparison(totalInvestment, portfolio))
+      setCostSF(totalInvestment/((portfolio+estNeeds)*year))
+    }
+  },[totalSavings,netSavings,totalInvestment,portfolio, costAvoidance, spaceOptimization])
+
   const handleClick = e => {
     switch(e.target.name){
       case "1":
-        setIsClicked1(!isClicked1)
+        setIsClicked1(true)
         setIsClicked2(false)
         setIsClicked3(false)
         setIsClicked4(false)
         setIsClicked5(false)
+        calculateSavings(1)
         break;
       case '2':
         setIsClicked1(false)
-        setIsClicked2(!isClicked2)
+        setIsClicked2(true)
         setIsClicked3(false)
         setIsClicked4(false)
         setIsClicked5(false)
+        calculateSavings(2)
         break;
       case '3':
         setIsClicked1(false)
         setIsClicked2(false)
-        setIsClicked3(!isClicked3)
+        setIsClicked3(true)
         setIsClicked4(false)
         setIsClicked5(false)
+        calculateSavings(3)
         break;
       case '4':
-        setIsClicked4(!isClicked4)
+        setIsClicked4(true)
         setIsClicked1(false)
         setIsClicked2(false)
         setIsClicked3(false)
         setIsClicked5(false)
+        calculateSavings(4)
         break;
       case '5':
-        setIsClicked5(!isClicked5)
+        setIsClicked5(true)
         setIsClicked1(false)
         setIsClicked2(false)
         setIsClicked3(false)
         setIsClicked4(false)
+        calculateSavings(5)
         break;
       default:
         
@@ -67,19 +117,41 @@ function App() {
           <Grid container spacing={4} justifyContent="center">
             <Grid item xs={10} md={5} sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
               <Typography variant='subtitle2' component='p' sx={{fontWeight: 700, fontSize:{sm:16,xs:12}}}>MONTHLY LEASE COST</Typography>
-              <TextField sx={{width: "100%"}} inputProps={{style: {fontSize: 20}}} placeholder='$00.0' variant='standard'/>
+              <TextField 
+                sx={{width: "100%"}} 
+                inputProps={{style: {fontSize: 20}}} 
+                placeholder='$00.0' 
+                variant='standard'
+                onChange={e => setLeaseCost(parseFloat(e.target.value))}/>
             </Grid>
             <Grid item xs={10} md={5} sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
               <Typography variant='subtitle2' component='p' sx={{fontWeight: 700, fontSize:{sm:16,xs:12}}}>TOTAL PORTFOLIO</Typography>
-              <TextField sx={{width: "100%"}} inputProps={{style: {fontSize: 20}}} placeholder='$00.0' variant='standard'/>
+              <TextField 
+                sx={{width: "100%"}} 
+                inputProps={{style: {fontSize: 20}}} 
+                placeholder='$00.0' 
+                variant='standard'
+                onChange={e => setPortfolio(parseFloat(e.target.value))}/>
             </Grid>
             <Grid item xs={10} md={5} sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
               <Typography variant='subtitle2' component='p' sx={{fontWeight: 700, fontSize:{sm:16,xs:12}}}>ANNUAL CAPEX SPEND</Typography>
-              <TextField sx={{width: "100%"}} inputProps={{style: {fontSize: 20}}} placeholder='$00.0' variant='standard'/>
+              <TextField 
+                sx={{width: "100%"}} 
+                inputProps={{style: {fontSize: 20}}} 
+                placeholder='$00.0' 
+                variant='standard'
+                onChange={e => setCapexSpend(parseFloat(e.target.value))}
+                />
             </Grid>
             <Grid item xs={10} md={5} sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
               <Typography variant='subtitle2' component='p' sx={{fontWeight: 700, fontSize:{sm:16,xs:12}}}>EST. NEW SQ.FT. NEEDS</Typography>
-              <TextField sx={{width: "100%"}} inputProps={{style: {fontSize: 20}}} placeholder='$00.0' variant='standard'/>
+              <TextField 
+                sx={{width: "100%"}} 
+                inputProps={{style: {fontSize: 20}}} 
+                placeholder='$00.0' 
+                variant='standard'
+                onChange={e => setEstNeeds(parseFloat(e.target.value))}
+                />
             </Grid>
           </Grid>
         </Box>
@@ -141,43 +213,43 @@ function App() {
           <Typography mt={1} component="h3" sx={{fontSize:{sm:"20px", md:"24", lg:"26px" }}} px={2} py={1} borderRadius={1} boxSizing="border-box" backgroundColor="black" color="white" fontWeight={900} className="heading">Savings</Typography>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>COST AVOIDANCE:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${costAvoidance.toLocaleString()}`}</Typography>
             </Box>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={1} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>SPACE OPTIMIZATION:<br />CAPEX COST SAVINGS</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${Math.round(spaceOptimization).toLocaleString()}`}</Typography>
             </Box>
           <Typography mt={1} component="h3" sx={{fontSize:{sm:"20px", md:"24", lg:"26px" }}} px={2} py={1} borderRadius={1} boxSizing="border-box" backgroundColor="black" color="white" fontWeight={900} className="heading">Return</Typography>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>TOTAL INVESTMENT:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${totalInvestment.toLocaleString()}`}</Typography>
             </Box>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>TOTAL SAVINGS:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>TOTAL SAVINGs:</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${Math.round(totalSavings).toLocaleString()}`}</Typography>
             </Box>
             <Divider />
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>NET SAVINGS:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${netSavings.toLocaleString()}`}</Typography>
             </Box>
           <Typography mt={1} component="h3" sx={{fontSize:{sm:"20px", md:"24", lg:"26px" }}} px={2} py={1} borderRadius={1} boxSizing="border-box" backgroundColor="black" color="white" fontWeight={900} className="heading">Estimations</Typography>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>ROI:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`${Math.round(roi*100)}%`}</Typography>
             </Box>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={1} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>COST/SF:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${Math.fround(costSF).toFixed(2)}`}</Typography>
             </Box>
           <Typography mt={1} component="h3" sx={{fontSize:{sm:"20px", md:"24", lg:"26px" }}} px={2} py={1} borderRadius={1} boxSizing="border-box" backgroundColor="black" color="white" fontWeight={900} className="heading">Competitor Comparison</Typography>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>DIRECT SAVINGS W/ AT:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`$${Math.fround(directSavings).toFixed(2)}`}</Typography>
             </Box>
             <Box width="100%" sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
               <Typography component="p" lineHeight={1} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>%SAVINGS COMPARISON:</Typography>
-              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>$ 00.0</Typography>
+              <Typography component="p" lineHeight={0.5} color="darkblue" fontFamily="Lato,'Lucida Grande',Verdana,sans-serif" fontSize={{md: 16}}>{`${Math.fround(savingsComparison*100).toFixed(2)}%`}</Typography>
             </Box>
         </Box>
       </Paper>
---
+++ src/calculations.js
@@ -0,0 +1,162 @@
+
+
+
+const miniInvestment = (val) => {
+    let p = []
+    let q = []
+    let r = [];  
+    let portfolio = val
+    const poc = 0
+    const upperSize = [1, 185000, 250000,500000, 750000, 1000000, 1500000, 2000000, 3000000, 4000000, 5000000]
+    const lowerSize = upperSize.map(size => (size+1))
+    const prices = [0.65, 0.65, 0.55, 0.50, 0.40, 0.33, 0.25, 0.22, 0.17, 0.14, 0.13, 0.12]
+
+    //find p
+    if(val>poc){ 
+      p.push(true)
+    }else{
+      p.push(false)
+    }
+    lowerSize.map(size => {
+      if(val>size){
+        p.push(true)
+      }else{
+        p.push(false)
+      }
+    })
+
+    // find q
+    upperSize.map(size => {
+      if(val > size){
+        q.push(false)
+      }else{
+        q.push(true)
+      }
+    })
+
+    for(let i = 0; i < q.length; i++){
+      if(p[i] === true){
+        if(q[i] === true){
+          r.push(true)
+        }else{
+          r.push(false)
+        }
+      }else{
+        r.push(false)
+      }
+    }
+    if(p[p.length] === true){
+      r.push(true)
+    }else{
+      r.push(false)
+    }
+
+    for(let i = 0; i < r.length; i++){
+      if(r[i]===true){
+        portfolio = val*prices[i]
+      }
+    }
+    
+    portfolio = (portfolio*0.9)*0.1
+  //   console.log('result ', Math.round(portfolio))
+    return Math.round(portfolio)
+}
+  
+  
+const findTotalInvestment = (val=0) => {
+  let p = []
+  let q = []
+  let r = [];  
+  let portfolio = val
+  const poc = 0
+  const upperSize = [1, 185000, 250000,500000, 750000, 1000000, 1500000, 2000000, 3000000, 4000000, 5000000]
+  const lowerSize = upperSize.map(size => (size+1))
+  const prices = [0.65, 0.65, 0.55, 0.50, 0.40, 0.33, 0.25, 0.22, 0.17, 0.14, 0.13, 0.12]
+
+  //find p
+  if(val>poc){ 
+      p.push(true)
+  }else{
+      p.push(false)
+  }
+  lowerSize.map(size => {
+      if(val>size){
+          p.push(true)
+      }else{
+          p.push(false)
+      }
+  })
+  
+  // find q
+  upperSize.map(size => {
+      if(val > size){
+          q.push(false)
+      }else{
+          q.push(true)
+      }
+  })
+
+  for(let i = 0; i < q.length; i++){
+    if(p[i] === true){
+      if(q[i] === true){
+        r.push(true)
+      }else{
+        r.push(false)
+      }
+    }else{
+      r.push(false)
+    }
+  }
+  if(p[p.length] === true){
+    r.push(true)
+  }else{
+    r.push(false)
+  }
+
+  for(let i = 0; i < r.length; i++){
+    if(r[i]===true){
+      portfolio = val*prices[i]
+    }
+  }
+  
+  // console.log('portfolio '+portfolio)
+  // console.log('k47 ',miniInvestment(val))
+  portfolio = (portfolio*0.9)+((val/1800)*(650))+miniInvestment(val)
+  // console.log('findTotalInvestment running...')
+  return Math.round(portfolio)
+}
+
+const findDirectSavings = (totalInvestment, portfolio) => {
+  const oneSensorPrice = 540
+  const avgSensorPrice = 590
+  const softwareCostPerSensor = 497
+  const implementationCost = 0.1
+
+  const sensors = portfolio/oneSensorPrice
+  const estimHardware = sensors*avgSensorPrice
+  const estimSoftware = softwareCostPerSensor*sensors
+  const implementation = estimSoftware*implementationCost
+
+  const totalPrice = estimHardware+estimSoftware+implementation
+
+  // console.log('findDirectSavings running...')
+  return (totalPrice-totalInvestment)
+}
+
+const findSavigsComparison = (totalInvestment, portfolio) => {
+  const oneSensorPrice = 540
+  const avgSensorPrice = 590
+  const softwareCostPerSensor = 497
+  const implementationCost = 0.1
+
+  const sensors = portfolio/oneSensorPrice
+  const estimHardware = sensors*avgSensorPrice
+  const estimSoftware = softwareCostPerSensor*sensors
+  const implementation = estimSoftware*implementationCost
+
+  const totalPrice = estimHardware+estimSoftware+implementation
+
+  return (1-(totalInvestment/totalPrice))
+}
+
+export {findTotalInvestment, findDirectSavings, findSavigsComparison}
